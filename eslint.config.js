2b299f8767ca964c2792c2f1a647d44dbcebcf36
Jonas Teixeira <jonaslagoteixeira@gmail.com>
2025-04-27 20:30:02 -0300

    feat: add eslint and prettier

---
+++ eslint.config.js
@@ -0,0 +1,39 @@
+import globals from 'globals'
+import eslint from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import reactX from 'eslint-plugin-react-x'
+import reactDom from 'eslint-plugin-react-dom'
+import reactHooks from 'eslint-plugin-react-hooks'
+import eslintConfigPrettier from 'eslint-config-prettier/flat'
+
+export default tseslint.config({
+  files: ['**/*.{ts,tsx}'],
+  ignores: ['dist', 'node_modules'],
+  extends: [
+    eslintConfigPrettier,
+    eslint.configs.recommended,
+    ...tseslint.configs.stylisticTypeChecked,
+    ...tseslint.configs.recommendedTypeChecked,
+  ],
+  plugins: {
+    js: eslint,
+    'react-x': reactX,
+    'react-dom': reactDom,
+    'react-hooks': reactHooks,
+  },
+  languageOptions: {
+    ecmaVersion: 2020,
+    globals: globals.browser,
+    parserOptions: {
+      project: ['./tsconfig.node.json', './tsconfig.app.json'],
+      tsconfigRootDir: import.meta.dirname,
+    },
+  },
+  rules: {
+    'no-unused-vars': 'warn',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    ...reactX.configs['recommended-typescript'].rules,
+    ...reactHooks.configs.recommended.rules,
+    ...reactDom.configs.recommended.rules,
+  },
+})
