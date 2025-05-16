2b3c372bf47db393cdda7a0cfa4816e18044934f
Jonas Teixeira <jonaslagoteixeira@gmail.com>
2025-05-15 22:16:35 -0300

    refactor: change validation place

--- src/components/FormUser.tsx
+++ src/components/FormUser.tsx
@@ -21,41 +21,7 @@ import { Switch } from './ui/switch'
 import { Checkbox } from './ui/checkbox'
 import { useUser } from '@/hooks/useUser'
 import { User } from '@/domain/user/types/user'
-
-const FormSchema = z.object({
-  name: z
-    .string({
-      required_error: 'Campo nome é obrigatório',
-    })
-    .min(2, {
-      message: 'Nome precisa conter pelo menos duas letras',
-    }),
-  email: z
-    .string({
-      required_error: 'Campo nome é obrigatório',
-    })
-    .email({ message: 'E-mail inválido' }),
-  phone: z
-    .string({
-      required_error: 'Campo telefone é obrigatório',
-    })
-    .min(14, { message: 'Formato inválido' })
-    .max(15, { message: 'Formato inválido' }),
-  cpf: z
-    .string({
-      required_error: 'Campo cpf é obrigatório',
-    })
-    .min(14, { message: 'Formato inválido' })
-    .max(14, { message: 'Formato inválido' }),
-  rg: z
-    .string({
-      required_error: 'Campo rg é obrigatório',
-    })
-    .min(6, { message: 'Formato inválido' })
-    .max(12, { message: 'Formato inválido' }),
-  active: z.boolean(),
-  whatsapp: z.boolean(),
-})
+import { FormSchema } from '@/utils/validations/user'
 
 export type UserSchema = z.infer<typeof FormSchema>
 
